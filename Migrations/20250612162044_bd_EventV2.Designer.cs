b12530acf514960c9f0d59c3dde54bd093dd08d0
Emilly Oliveira <emilly.o.santos7@aluno.senai.br>
2025-06-12 16:56:09 -0300

    feat: Atualizando metodo de listar

--- Migrations/20250612162044_bd_EventV2.Designer.cs
+++ Migrations/20250612162044_bd_EventV2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapi.event_.Contexts;
 
@@ -11,9 +12,11 @@ using webapi.event_.Contexts;
 namespace webapi.event_.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250612162044_bd_EventV2")]
+    partial class bd_EventV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
